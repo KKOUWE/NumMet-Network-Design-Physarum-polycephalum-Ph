9f87e4558c55ec17dc2a6853bcf7a27188ac4bd5
HetNieuweNormaal <floris.r.m.robert@gmail.com>
2024-11-08 13:39:18 +0100

    Clean up

--- STARTING POSISTION.py
+++ STARTING POSISTION.py
@@ -1,39 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as panda
+import networkx as nx
 
-## this file has to be able to add a stochastic array over a given map,
+## this file has to be able to add a stochastic array of points over a given map,
 ## together with foodsources (FS)
 
-Length = range(0,10,1)
-Height = range(0,10,1)
-x_list = []
-y_list = []
+# Create an empty graph
+G = nx.Graph()
 
-# Generating a matrix of points with some noise added to them with a for loop,
-# this still has to be able to be put over a map; will add later functionality.
-# 
-for u in Height:
-    for i in Length:
-        #make a point and add random noise to it np.random.normal()
-        x_list.append(u + np.random.normal(0, 0.1))
-        y_list.append(i + np.random.normal(0, 0.1))
+# Define the grid dimensions
+rows, cols = 10, 10
+min_distance = np.sqrt(2)
+# Add nodes in a 10x10 grid
+for i in range(rows):
+    for j in range(cols):
+        G.add_node((i, j))
+        # Set up positions for a 10x10 grid
+        # standerd deviation
+        sd = 0.2
+        pos = {(i, j): (j + np.random.normal(loc=0,scale=sd, size=None) , -i + np.random.normal(loc=0,scale=sd, size=None) ) for i in range(rows) for j in range(cols)}
+        # Check all other nodes in the grid to see if they meet the distance condition # lots of room for optimizatin in this loop
+        for x in range(rows):
+            for y in range(cols):
+                pos_center = pos.get((i,j))
+                pos_nb = pos.get((x,y))
+                if pos_nb != pos_center:
+                    pos_center_i = pos_center[0]
+                    pos_center_j = pos_center[1]
+                    pos_nb_i = pos_nb[0]
+                    pos_nb_j = pos_nb[1]
+                # Calculate distance
+                    distance = np.sqrt((pos_center_i- pos_nb_i) ** 2 + (pos_center_j - pos_nb_j) ** 2)
+                    if distance <= min_distance:
+                        G.add_edge((i, j), (x, y))
 
-# plot the points by extracting from the library
-plt.scatter(x_list, y_list, marker='.', linewidths = 0.01, c='red')
-# For every point [x,y] we will connect it to its right- and below-neighbour using plot
-for o in x_list:
-    for p in y_list:
-        # define neighbours
-        nb_center = [o, p]
-        nb_right = [o, p+1]
-        nb_below = [o+1, p]
 
-        ## Drawing the lines between neighbours using plot. While keeping in mind the limits of the matrix
-        # draw HORIZONTAL line
-        if nb_center[1]<len(Length):
-            plt.plot(nb_center, nb_right, mfc='red', markersize=0.1,)
-        # draw VERTICAL line
-        if nb_center[0]<len(Height):
-            plt.plot(nb_center, nb_below, mfc='red', markersize=0.1,)
+
+# Draw the graph
+plt.figure()
+nx.draw(G, pos, node_size=5, node_color="red", with_labels=False)
 plt.show()
+## It works !!
+
+## Now we need to implement food sources = FS and the tube size = TS. Also this tube size has to 
+# change with every iteration dt. according to 
